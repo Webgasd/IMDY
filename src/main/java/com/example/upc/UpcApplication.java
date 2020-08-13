688b374de09a4fb61a327ceab0cd5f117e0c3af9
wenghaonan <wx_ae5873b9e90e46448f55e0851286313b@git.code.tencent.com>
2020-08-14 00:48:50 +0800

    update whn password 2020/2023/

--- src/main/java/com/example/upc/UpcApplication.java
+++ src/main/java/com/example/upc/UpcApplication.java
@@ -3,9 +3,11 @@ package com.example.upc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages={"com.example.upc"})
 @MapperScan("com.example.upc.dao")
+@EnableScheduling
 public class UpcApplication {
     public static void main(String[] args) {
         SpringApplication.run(UpcApplication.class, args);
